c523af7624fdb1940c1fcfe09ee5d56bc094ebe7
austin <austin@192.168.1.106>
2018-09-22 15:34:55 +0800

    我舉辦的活動-無法顯示已下架問題bug

--- demo/src/main/java/com/example/demo/service/ActivityService.java
+++ demo/src/main/java/com/example/demo/service/ActivityService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
 import com.example.demo.entity.TActivity;
 import com.example.demo.entity.TActivityType;
 import com.example.demo.entity.TTicket;
@@ -18,60 +19,60 @@ import com.example.demo.utils.CurrentUser;
 
 @Service
 public class ActivityService {
-	
+
 	@Value("${activity.file.path}")
 	private String imageFilePath;
-	
+
 	@Autowired
 	private TActivityRepository activityRepository;
-	
+
 	@Autowired
 	private TTicketRepository ticketRepository;
-	
+
 	@Autowired
 	private TActivityTypeRepository activitytypeRepository;
-	
+
 	@Autowired
 	private CurrentUser currentUser;
-		
+
 	public Iterable<TActivity> findAll() {
 		return activityRepository.findAll();
 	}
-	
+
 	public TActivity getActivityById(int id) {
 		return activityRepository.getById(id);
 	}
 
-	public  List<TActivityType> getActivityType() {
+	public List<TActivityType> getActivityType() {
 		return activitytypeRepository.getAllTActivityType();
 	}
-	
+
 	public TActivity getActivityByIdAndCurrentUser(int id) {
 		return activityRepository.getByIdAndUser(id, currentUser.getUserId());
 	}
-	
-	
-	public TActivity createByImage(TActivity activity,TTicket ticket, MultipartFile file) throws IllegalStateException, IOException {
-		
-		//建立活動資料
+
+	public TActivity createByImage(TActivity activity, TTicket ticket, MultipartFile file)
+			throws IllegalStateException, IOException {
+
+		// 建立活動資料
 		activity.setActivityImage(file.getOriginalFilename());
 		TActivity rs = this.create(activity, ticket);
-		
-        
-        //開專屬目錄
-        File dir = new File(imageFilePath + "/" + rs.getActivityId());
-   	    if (!dir.exists()) {
-  	      dir.mkdir();
-  	    }
-   	    
-   	    File uploadFile = new File(dir.getPath() + "/" +file.getOriginalFilename());
-		
-   	    //上傳檔案
-   	    file.transferTo(uploadFile);
-   	 
-   	    return rs;
-   	    
+
+		// 開專屬目錄
+		File dir = new File(imageFilePath + "/" + rs.getActivityId());
+		if (!dir.exists()) {
+			dir.mkdir();
+		}
+
+		File uploadFile = new File(dir.getPath() + "/" + file.getOriginalFilename());
+
+		// 上傳檔案
+		file.transferTo(uploadFile);
+
+		return rs;
+
 	}
+
 	public List<TActivity> selectAll() {
 		return activityRepository.getAllActivity();
 	}
@@ -80,45 +81,48 @@ public class ActivityService {
 		return activityRepository.getActivityBySearch(sValue);
 	}
 
-	//審核活動狀態
+	// 審核活動狀態
 	public List<TActivity> selectPass() {
 		return activityRepository.getActivityByStatus("2");
 	}
+
 	public List<TActivity> selectCheck() {
 		return activityRepository.getActivityByStatus("0");
 	}
+
 	public List<TActivity> selectWaitCheck() {
 		return activityRepository.getActivityByStatus("1");
 	}
+
 	public List<TActivity> selectCancel() {
-		return activityRepository.getActivityByStatus("9");
-	}	
+		return activityRepository.getActivityByStatus("8");
+	}
+
 	public List<TActivity> faultCheck() {
 		return activityRepository.getActivityByStatus("3");
 	}
-	
+
 	public List<TActivityType> getAllTActivityType() {
 		return activitytypeRepository.getAllTActivityType();
-	}	
-	
-	public TActivity create(TActivity activity,TTicket ticket) {
+	}
+
+	public TActivity create(TActivity activity, TTicket ticket) {
 		activity.setActivityHost(currentUser.getUserName());
 		activity.setCreateUser(currentUser.getUserId());
 		activity.setUpdateUser(currentUser.getUserId());
-		
+
 		TActivity rs = activityRepository.save(activity);
-		if(rs != null) {
+		if (rs != null) {
 			ticket.setActivityId(rs.getActivityId());
-		    ticketRepository.save(ticket);
+			ticketRepository.save(ticket);
 		}
-		
+
 		return rs;
 	}
-	
-	
+
 	public void update(TActivity activity) {
 		TActivity db = activityRepository.getById(activity.getActivityId());
-	
+
 		db.setActivityName(activity.getActivityName());
 		db.setActivityDate(activity.getActivityDate());
 		db.setEndTime(activity.getEndTime());
@@ -130,76 +134,80 @@ public class ActivityService {
 		db.setActivityType(activity.getActivityType());
 		db.setHostMail(activity.getHostMail());
 		db.setHostTel(activity.getHostTel());
-		
+
 		activityRepository.save(db);
 	}
-	
-	//審核活動
+
+	// 審核活動
 	public void updateSendExamine(int activityId) {
 		TActivity db = activityRepository.getByIdAndUser(activityId, currentUser.getUserId());
-	
-		if(db != null) {
+
+		if (db != null) {
 			db.setActivityStatus("1");
 			activityRepository.save(db);
 		}
 	}
-	
+
 	public void updateCancel(int activityId) {
 		TActivity db = activityRepository.getByIdAndUser(activityId, currentUser.getUserId());
-	
-		if(db != null) {
+
+		if (db != null) {
 			db.setActivityStatus("9");
 			activityRepository.save(db);
 		}
 	}
-	
+
 	public void updateExaminePass(int activityId) {
-		
-	//	if(StringUtils.equals(currentUser.getUserRole(), "M")) {
-			TActivity db = activityRepository.getById(activityId);
-		
-			if(db != null) {
-				db.setActivityStatus("2");
-				activityRepository.save(db);
-			}
-	//	}
+
+		// if(StringUtils.equals(currentUser.getUserRole(), "M")) {
+		TActivity db = activityRepository.getById(activityId);
+
+		if (db != null) {
+			db.setActivityStatus("2");
+			activityRepository.save(db);
+		}
+		// }
 	}
-	
+
 	public void updateExamineFail(int activityId) {
-		
-			TActivity db = activityRepository.getById(activityId);
-		
-			if(db != null) {
-				db.setActivityStatus("3");
-				activityRepository.save(db);
-			}
+
+		TActivity db = activityRepository.getById(activityId);
+
+		if (db != null) {
+			db.setActivityStatus("3");
+			activityRepository.save(db);
+		}
 	}
+
 	// 我舉辦的活動
 	public List<TActivity> selectCreateUserPass() {
 		return activityRepository.getAllCreateUser(currentUser.getUserId(), "2");
 	}
- 	public List<TActivity> selectCreateUserWaitCheck() {
+
+	public List<TActivity> selectCreateUserWaitCheck() {
 		return activityRepository.getAllCreateUser(currentUser.getUserId(), "1");
 	}
- 	public List<TActivity> selectCreateUserEnd() {
+
+	public List<TActivity> selectCreateUserEnd() {
 		return activityRepository.getAllCreateUser(currentUser.getUserId(), "8");
 	}
- 	public List<TActivity> selectCreateUserCancel() {
+
+	public List<TActivity> selectCreateUserCancel() {
 		return activityRepository.getAllCreateUser(currentUser.getUserId(), "9");
 	}
- 	
+
 	public void updateEnd(int activityId) {
 		TActivity db = activityRepository.getById(activityId);
- 		if (db != null) {
+		if (db != null) {
 			db.setActivityStatus("8");
 			activityRepository.save(db);
 		}
 	}
-	
+
 	public void createtype(TActivityType type) {
 
-//		TActivityType rs = activitytypeRepository.save(type);
+		// TActivityType rs = activitytypeRepository.save(type);
 		this.activitytypeRepository.save(type);
 	}
-	
+
 }
